4dec859bbc3b79d0af00e36a8d609ee0c995ecce
Robin Böhm <robinboehm@googlemail.com>
2013-08-27 20:49:31 +0200

    Exted filter function

--- app/scripts/controllers/ShameCtrl.js
+++ app/scripts/controllers/ShameCtrl.js
@@ -9,10 +9,22 @@ angular.module("angular-mobile-docs")
         $scope.api = AngularGitFetchService.getAPI($scope.config.version);
 
 
-        $scope.filterVersions = function(data){
-            data.select(function(item){
-               return (item.name.match(/^(1.0.[2-9])|(1.[1-9][0-9]?.\w+)/));
-            });
+        $scope.filterVersions = function(dataList){
+            return true;
+            if(angular.isUndefined(data)){
+                console.log("data",data);
+                return true;
+            }
+
+            var resultSet = [];
+            for (var i = 0; i < data.length; i++){
+                var item = data[i];
+                if(!!(item.name.match(/^(1.0.[2-9])|(1.[1-9][0-9]?.\w+)/))){
+                    resultSet.push(item);
+                }
+            }
+
+            return resultSet;
         }
 
         $scope.setVersion = function(name){
