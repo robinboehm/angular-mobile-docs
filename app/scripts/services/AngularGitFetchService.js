635dbffef0382b0a30d42d72f98b63731fa71e27
Robin Böhm <robinboehm@googlemail.com>
2013-08-27 20:48:02 +0200

    service use mock data now

--- app/scripts/services/AngularGitFetchService.js
+++ app/scripts/services/AngularGitFetchService.js
@@ -8,11 +8,13 @@ angular.module("angular-mobile-docs")
         }
 
         var getVersionList = function () {
-            return get("https://api.github.com/repos/angular/code.angularjs.org/contents/");
+            //return get("https://api.github.com/repos/angular/code.angularjs.org/contents/");
+            return get("mock/versions.json");
         }
 
         var getAPI = function(version){
-            return get("https://api.github.com/repos/angular/code.angularjs.org/contents/"+version+"/docs/partials/api/?ref=master");
+            //return get("https://api.github.com/repos/angular/code.angularjs.org/contents/"+version+"/docs/partials/api/?ref=master");
+            return get("mock/api.json");
         }
 
         //https://github.com/angular/code.angularjs.org/blob/master/1.2.0rc1/docs/docs-data.js
