6f9e7588a82b6350b3bc9c8b0dc5f39ed9390ffd
Robin Böhm <robinboehm@googlemail.com>
2013-08-27 20:54:06 +0200

    add regex filter for version 1.0.2+

--- app/scripts/controllers/ShameCtrl.js
+++ app/scripts/controllers/ShameCtrl.js
@@ -9,22 +9,8 @@ angular.module("angular-mobile-docs")
         $scope.api = AngularGitFetchService.getAPI($scope.config.version);
 
 
-        $scope.filterVersions = function(dataList){
-            return true;
-            if(angular.isUndefined(data)){
-                console.log("data",data);
-                return true;
-            }
-
-            var resultSet = [];
-            for (var i = 0; i < data.length; i++){
-                var item = data[i];
-                if(!!(item.name.match(/^(1.0.[2-9])|(1.[1-9][0-9]?.\w+)/))){
-                    resultSet.push(item);
-                }
-            }
-
-            return resultSet;
+        $scope.filterVersions = function(item){
+            return !!(item.name.match(/^(1.0.[2-9])|(1.[1-9][0-9]?.\w+)/));
         }
 
         $scope.setVersion = function(name){
