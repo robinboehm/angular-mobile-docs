5f2d969c2bd29f6ddc6416908d122bb63707b6a5
Sascha Brink <sascha.brink@gmail.com>
2013-08-27 21:59:51 +0200

    Add router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,9 @@
 'use strict'
 
-angular.module("angular-mobile-docs",["ngRoute","ngAnimate"]);
+angular.module("angular-mobile-docs", ["ngRoute","ngAnimate"])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/api/:version', { templateUrl: 'api.html', controller: 'ShameCtrl' })
+      .otherwise({ redirectTo: '/api/1.1.0' });
+
+  });
