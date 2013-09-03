49207d3665ee415b445b0d5370888762ff793aad
Robin Böhm <robinboehm@googlemail.com>
2013-09-03 21:46:39 +0200

    Add Prettify deps to index and app.js

--- app/www/scripts/app.js
+++ app/www/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module("angular-mobile-docs", ["ngRoute", "ngAnimate", "ngSanitize", "ngTouch"])
+angular.module("angular-mobile-docs", ["ngRoute", "ngAnimate", "ngSanitize", "ngTouch", 'bootstrap', 'bootstrapPrettify'])
     .constant("baseUrl", "http://apidocs.angularjs.de/")
     .constant("initVersion", "1.2.0rc1")
     .config(function ($routeProvider, initVersion) {
