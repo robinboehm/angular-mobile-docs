49a4ccd7a911677bd09fe362b8d37f9447c3e06c
Robin Böhm <robinboehm@googlemail.com>
2013-09-14 14:33:40 +0200

    Extract compression to an own service layer

--- app/www/scripts/services/LocalStorageCache.js
+++ app/www/scripts/services/LocalStorageCache.js
@@ -1,19 +1,19 @@
 angular.module('angular-mobile-docs').
-    factory('LocalStorageCache', function ($cacheFactory) {
+    factory('LocalStorageCache', function ($cacheFactory, Compressor) {
         var cache = $cacheFactory('LocalStorageCache', {});
         cache.get = function (key) {
             var item = localStorage.getItem(key);
             if (!item) return; // Cache miss
-            item = JSON.parse(LZString.decompress(item));
+            item = JSON.parse(Compressor.decompress(item));
             return item.data; // Cache hit
         };
         cache.put = function (key, value) {
             if (typeof value.then === 'function') {
                 value.then(function (value) {
-                    localStorage.setItem(key, LZString.compress(JSON.stringify(value)));
+                    localStorage.setItem(key, Compressor.compress(JSON.stringify(value)));
                 });
             } else {
-                localStorage.setItem(key, LZString.compress(JSON.stringify(value)));
+                localStorage.setItem(key, Compressor.compress(JSON.stringify(value)));
             }
         };
         cache.remove = function (key) {
